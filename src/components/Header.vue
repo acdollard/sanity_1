0016547465560df586fc95a923b7d3ab1c9bae1a
Alexander Dollard <alexdollard141@gmail.com>
2024-10-03 20:57:21 -0500

    looking good

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,15 +10,15 @@ const toggleOpen = () => {
 </script>
 
 <template>
-  <header class="bg-slate-100 mb-4 pt-4 pb-2 sm:flex sm:justify-between sm:items-center">
+  <header class="bg-slate-100 mb-4 pt-4 pb-2 sm:flex sm:justify-between sm:items-center sticky">
 
     <div class="flex items-center justify-between px-4">
       <div class="p-2">
-        <a href="/" class="text-lg transition-all ease-in">Hillsboro Web Designs</a>
+        <h2 class="text-gradient text-3xl"><a href="/">Hillsboro Web Designs</a></h2>
       </div>
     
       <div class="flex items-center sm:hidden">
-        <button @click="toggleOpen" class="p-2 transition-all hover:bg-slate-600 hover:text-slate-100 rounded">
+        <button @click="toggleOpen" class="p-2 transition-all hover:font-bold rounded">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             class="w-6 h-6"
@@ -35,14 +35,25 @@ const toggleOpen = () => {
       </div>
     </div>
 
-    <div :class="isOpen ? 'block' : 'hidden'" class="px-2 sm:flex ">
-      <a href="/about" class="px-4 py-2 block transition ease-in delay-75 hover:bg-slate-600 hover:text-slate-100 rounded ">About</a>
-      <a href="/contact" class="px-4 py-2 block transition ease-in delay-75 hover:bg-slate-600 hover:text-slate-100 rounded  ">Contact</a>
-      <a href="#" class="px-4 py-2 block transition ease-in delay-75 hover:bg-slate-600 hover:text-slate-100 rounded ">Portfolio</a>
+    <div :class="isOpen ? 'flex' : 'hidden'" class="px-2 sm:flex items-center justify-center">
+      <a href="/about" class="px-4 py-2 block transition ease-in delay-75 hover:font-bold rounded ">About</a>
+      <a href="/contact" class="px-4 py-2 block transition ease-in delay-75 hover:font-bold rounded">Contact</a>
+      <a href="#" class="px-4 py-2 block transition ease-in delay-75 hover:font-bold rounded">Portfolio</a>
     </div>
 
   </header>
 </template>
+
+<style scoped>
+  .text-gradient {
+    background-image: var(--accent-gradient);
+    -webkit-background-clip: text;
+    background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-size: 400%;
+    background-position: 0%;
+  }
+</style>
 
 
 
