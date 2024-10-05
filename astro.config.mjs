6d2834b564683781e6558dfc5fad6db4224bc840
Alexander Dollard <alexdollard141@gmail.com>
2024-10-05 08:00:35 -0500

    updates site title

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,11 @@ import tailwind from '@astrojs/tailwind';
 
 import vue from '@astrojs/vue';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()]
+  integrations: [tailwind(), vue()],
+  output: 'server',
+  adapter: netlify()
 });
