d67f53c842a6382c310d3a4e561005c179442836
Alexander Dollard <alexdollard141@gmail.com>
2024-10-09 11:06:52 -0500

    missed footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="footer w-full bg-seasalt h-24 font-jakarta flex justify-center items-center z-10">
-    <p>	&copy; <a href="/">Copyright 2024 | Hillsboro Web Designs</a></p>
+    <p>	&copy; <a href="/">Copyright 2024 | Hillsboro Digital</a></p>
   </div>
 </template>
 
