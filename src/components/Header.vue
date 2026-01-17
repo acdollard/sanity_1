9a967881866d8741aebc2b0ec25d71405fe2001f
Alexander Dollard <alexdollard141@gmail.com>
2026-01-17 13:40:21 -0600

    darkens header and adds dope lightning bolt

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -14,7 +14,15 @@ const toggleOpen = () => {
 
     <div class="flex items-center justify-between px-4">
       <div class="p-2">
-        <h2 class="text-gradient text-2xl"><a href="/">Hillsboro Digital</a></h2>
+        <h2 class="text-gradient text-2xl">
+          <a href="/" class="flex items-center gap-2">
+            Hillsboro
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 lightning-bolt">
+              <path fill-rule="evenodd" d="M14.615 1.595a.75.75 0 01.359.852L12.982 9.75h7.268a.75.75 0 01.548 1.262l-10.5 11.25a.75.75 0 01-1.272-.71l1.992-7.302H3.75a.75.75 0 01-.548-1.262l10.5-11.25a.75.75 0 01.913-.143z" clip-rule="evenodd" />
+            </svg>
+            Digital
+          </a>
+        </h2>
       </div>
     
       <div class="flex items-center sm:hidden">
@@ -64,6 +72,10 @@ const toggleOpen = () => {
     -webkit-text-fill-color: transparent;
     background-size: 400%;
     background-position: 0%;
+  }
+  
+  .lightning-bolt {
+    fill: rgb(var(--accent2-dark));
   }
 </style>
 
